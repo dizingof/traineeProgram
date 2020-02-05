e076274e02cbb3660b5e55ea6d6bc5565441673c
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-05 15:41:42 +0200

    Add project files.

---
+++ stringToHtml/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringToHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "Project: P1, Project: P2";
+            string removeStr = ", Project";
+            int n = inputStr.IndexOf(removeStr);
+            inputStr = inputStr.Remove(n, removeStr.Length);
+            var splitStr = inputStr.Split(':');
+
+            foreach (var c in splitStr)
+            {
+                Console.WriteLine(c);
+            }
+            //Console.Write("<ul>");
+            //foreach (string c in splitStr)
+            //{
+            //    Console.WriteLine($"<li>{c}</li>");
+            //}
+            //Console.Write("<ul>");
+            Console.ReadKey();
+        }
+    }
+}
