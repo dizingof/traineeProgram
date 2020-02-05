b8e0874d5c609510c2559fde14ca50d59543a609
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-05 18:36:18 +0200

    create method for inputstr convert to html outputstr

--- stringToHtml/Program.cs
+++ stringToHtml/Program.cs
@@ -10,24 +10,50 @@ namespace stringToHtml
     {
         static void Main(string[] args)
         {
-            string inputStr = "Project: P1, Project: P2";
-            string removeStr = ", Project:";
-            int n = inputStr.IndexOf(removeStr);
-            inputStr = inputStr.Remove(n, removeStr.Length);
-            string removeStr2 = "Project: ";
-            int n2 = inputStr.IndexOf(removeStr2);
-            inputStr = inputStr.Remove(n2, removeStr2.Length);
+            string convertStringToHtml(string inputStr, string removeStr)
+            {
 
+                int n = inputStr.IndexOf(removeStr);
+                inputStr = inputStr.Remove(n, removeStr.Length);
+                string [] splitStr = inputStr.Split(' ');
+                var resultString = string.Join(" ", splitStr);
+                return resultString;
+            }
 
-            var splitStr = inputStr.Split(' ');
+            var result = convertStringToHtml("Project: P1, Project: P2", ", Project:");
+            string result1 = convertStringToHtml(result, "Project: ");
+            var resultSplit = result1.Split(' ');
+
             Console.Write("Project");
             Console.Write("<ul>");
-            foreach (string c in splitStr)
+            foreach (var c in resultSplit)
             {
                 Console.Write($"<li>{c}</li>");
             }
             Console.Write("</ul>");
             Console.ReadKey();
+
+
+
+
+
+
+            //string inputStr = "Project: P1, Project: P2";
+            //string removeStr = ", Project:";
+            //int n = inputStr.IndexOf(removeStr);
+            //inputStr = inputStr.Remove(n, removeStr.Length);
+            //string removeStr2 = "Project: ";
+            //int n2 = inputStr.IndexOf(removeStr2);
+            //inputStr = inputStr.Remove(n2, removeStr2.Length);
+            //var splitStr = inputStr.Split(' ');
+            //Console.Write("Project");
+            //Console.Write("<ul>");
+            //foreach (string c in splitStr)
+            //{
+            //    Console.Write($"<li>{c}</li>");
+            //}
+            //Console.Write("</ul>");
+            //Console.ReadKey();
         }
     }
 }
