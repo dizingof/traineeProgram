13e6f3aa961ac27807d9646c216618a93997061d
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-07 12:25:39 +0200

    add console enter

--- stringToHtml/stringToHtml.cs
+++ stringToHtml/stringToHtml.cs
@@ -10,16 +10,22 @@ namespace stringToHtml
     {
         static void Main(string[] args)
         {
-            var finish = buildStrOutputResult();
+            string resultEnterData = EnterData();
+            var finish = BuildOutputResult(resultEnterData);
             Console.WriteLine(finish);
             Console.ReadKey();
 
         }
-       
-        public static string buildStrOutputResult()
+        public static string EnterData()
         {
-            string firstResult = convertStringToHtml("Project: P1, Project: P2", ", Project:");
-            string secondResult = convertStringToHtml(firstResult, "Project: ");
+            Console.WriteLine("Vvedite dannie v formate: \"Project: P1, Project: P2\"");
+            string enteredData = Console.ReadLine();
+            return enteredData;
+        }
+        public static string BuildOutputResult(string resultEnterData)
+        {
+            string firstResult = RemoveWordFromEnteredData(resultEnterData, ", Project:");
+            string secondResult = RemoveWordFromEnteredData(firstResult, "Project: ");
             var resultAfterSplit = secondResult.Split(' ');
             string strOutPut = string.Empty;
             foreach (var item in resultAfterSplit)
@@ -29,11 +35,12 @@ namespace stringToHtml
             string strOutputResult = $"Project<ul>{strOutPut}</ul>";
             return strOutputResult;
         }
-        public static string convertStringToHtml(string inputStr, string removeStr)
+        public static string RemoveWordFromEnteredData(string inputStr, string removeStr)
         {
             int number = inputStr.IndexOf(removeStr);
             var inputStrAfterRemove = inputStr.Remove(number, removeStr.Length);
             return inputStrAfterRemove;
         }
+        
     }
 }
