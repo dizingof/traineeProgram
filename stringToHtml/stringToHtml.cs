19cf8c04fe4a5337d7e19df52bd487d46c3bee15
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-19 14:13:04 +0200

    Метод ConvertToHtml выводит список, добавлен аут параметр в метод VvodMasiva

--- stringToHtml/stringToHtml.cs
+++ stringToHtml/stringToHtml.cs
@@ -12,19 +12,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            var enteredArray = VvodMasiva();
-            ChooseOptionOfOutput(enteredArray);
+            var enteredArray = VvodMasiva(out int dlinnaMasivaI);
+            ChooseOptionOfOutput(enteredArray, dlinnaMasivaI);
             Console.ReadKey();
         }
+       
 
-
-        public static string[,] VvodMasiva()
+        public static string[,] VvodMasiva(out int dlinnaMasivaI)
         {
-            try
-            {
+            
                 Console.Write("vvedite kolichestvo proectov:\t");
                 int elementsCountI = int.Parse(Console.ReadLine());
-
+                dlinnaMasivaI = elementsCountI;
                 int elementsCountJ = 3;
                 string[,] myArray = new string[elementsCountI, elementsCountJ];
 
@@ -47,29 +46,23 @@ namespace ConsoleApp1
                     {
 
                         Console.Write(myArray[i, j] + "\t" + "\t" + "\t");
-
-
-
+                                       
                     }
                     Console.WriteLine("\n");
                 }
-                return myArray;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"erorr: {ex.ToString()}");
-                return null;
-            }
+                return (myArray);
+            
+           
 
         }
-        public static void ChooseOptionOfOutput(string[,] enteredArray)
+        public static void ChooseOptionOfOutput(string[,] enteredArray, int dlinnaMasivaI)
         {
             Console.WriteLine("Vvedite 1 - dlya vivoda v formate (List), 2 - dlya vivoda v formate (Table), 3 - dlya vivoda v formate (Json)");
             string numberChoiseOption = Console.ReadLine();
             switch (numberChoiseOption)
             {
                 case "1":
-                    var afterConvert = ConvertToHtml(enteredArray);
+                    var afterConvert = ConvertToHtml(enteredArray, dlinnaMasivaI);
                     WriteToFile(afterConvert);
                     break;
                 case "2":
@@ -84,25 +77,34 @@ namespace ConsoleApp1
 
         }
 
-        public static string ConvertToHtml(string[,] myArray)
+        public static string ConvertToHtml(string[,] myArray, int elementsCountI)
         {
             try
             {
                 string strOutPut = string.Empty;
                 string strOutPut1 = string.Empty;
+              
                 //foreach (var item in myArray)
                 //{
                 //    strOutPut += $"<li>Project:</li><li>{item}</li>";
                 //}
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < elementsCountI; i++)
                 {
+                    strOutPut = null;
                     for (int j = 0; j < 3; j++)
                     {
+                        
                         strOutPut += $"<li>{myArray[i, j]}</li>";
+                        
                     }
-                    strOutPut1 += $"<li>Project:</li>" + strOutPut;
+                    
+                        strOutPut1 += $"<li>Project:</li>" + strOutPut;
+                    
+                    
+
                 }
                
+
                 string strOutputResult = $"<ul>{strOutPut1}</ul>";
                 Console.WriteLine(strOutputResult);
                 return strOutputResult;
