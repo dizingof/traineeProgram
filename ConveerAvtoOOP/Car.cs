5673156ac58a7b238d42aa5cfc4f1f6682b14990
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-26 18:03:06 +0200

    iteration 1

---
+++ ConveerAvtoOOP/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConveerAvtoOOP
+{
+    public class Car
+    {
+        public string Dvigatel { get; set; }
+        public string Color { get; set; }
+        public string Kuzov { get; set; }
+
+    }
+}
---
+++ ConveerAvtoOOP/Conveer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConveerAvtoOOP
+{
+    class Conveer
+    {
+        Dvigatel dieselDvigatel = new Dvigatel() { Type= "disel", Сapacity = 1000 };
+        Color cvet = new Color() { Name = "Sinii" };
+        
+        public static Car CreateCar(Dvigatel dieselDvigatel, Color cvet)
+        {
+            Car dieselCar = new Car() { Dvigatel = dieselDvigatel.Type, Color = cvet.Name };
+            return dieselCar;
+        }
+    }
+}
