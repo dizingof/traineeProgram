de1623d9e99ee6050977bdfd6f4acb72608ee82a
Roman <Roman@DESKTOP-17DJS6O>
2020-03-03 10:51:43 +0200

    add nasledovanie

---
+++ Nasledovanie/baseHuman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledovanie
+{
+    class baseHuman
+    {
+        public string golova;
+        public string ruki;
+        public string nogi;
+
+        public void Spat()
+        {
+            Console.WriteLine("baseHuman spit");
+        }
+        public void UpotrevlyatEdu()
+        {
+            Console.WriteLine("baseHuman upotreblyaet edu");
+        }
+
+        public baseHuman(string golova, string ruki, string nogi)
+        {
+            this.golova = golova;
+            this.ruki = ruki;
+            this.nogi = nogi;
+        }
+    }
+}
---
+++ PeregruzkaMetodov/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeregruzkaMetodov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Add(2, 6);
+            calc.Add(2.5, 6.3);
+            Console.ReadLine();
+        }
+
+        class Calculator
+        {
+            public void Add(int a, int b)
+            {
+                int result = a + b;
+                Console.WriteLine($"Result is {result}");
+            }
+            public void Add(int a, int b, int c)
+            {
+                int result = a + b + c;
+                Console.WriteLine($"Result is {result}");
+            }
+            public int Add(int a, int b, int c, int d)
+            {
+                int result = a + b + c + d;
+                Console.WriteLine($"Result is {result}");
+                return result;
+            }
+            public void Add(double a, double b)
+            {
+                double result = a + b;
+                Console.WriteLine($"Result is {result}");
+            }
+        }
+    }
+}
