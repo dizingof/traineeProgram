7a9d3f9db734cc49c719157c866eeed3df105d73
Roman <Roman@DESKTOP-17DJS6O>
2020-03-04 21:05:46 +0200

    add  compare struct and class

---
+++ Struct/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new A(1, 2);
+            var b = new A(1, 2);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            Console.WriteLine(a.Equals(b));
+            Console.ReadLine();
+
+            //Структуры сравниваются по значениям полей
+            //Классы сравниваются по ссылке на кучу
+        }
+    }
+
+    class A
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public A(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
