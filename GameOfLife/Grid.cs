4b97f3d3511975a0e3e3553cac1a4706d8925ca7
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-03-10 22:27:51 +0200

    1

--- GameOfLife/Grid.cs
+++ GameOfLife/Grid.cs
@@ -9,8 +9,23 @@ namespace GameOfLife
     class Grid
     {
         private Cell[,] space;
-        public int dlina = 10;
-        public int shirina = 10;
+        public bool[,] mass = new bool[50, 50];
+
+
+        private int sosediCount(int a, int b)
+        {
+            int sosedi = 0;
+            if (a > 0 && mass[a - 1, b]) { sosedi++; }
+            if (a > 0 && b < 49 && mass[a - 1, b + 1]) { sosedi++; }
+            if (b < 49 && mass[a, b + 1]) { sosedi++; };
+            if (a < 49 && b < 49 && mass[a + 1, b + 1]) { sosedi++; }
+            if (a < 49 && mass[a + 1, b]) { sosedi++; }
+            if (a < 49 && b > 0 && mass[a + 1, b - 1]) { sosedi++; }
+            if (b > 0 && mass[a, b - 1]) { sosedi++; }
+            if (a > 0 && b > 0 && mass[a - 1, b - 1]) { sosedi++; }
+            return sosedi;
+        }
+
+        
     }
-   
 }
