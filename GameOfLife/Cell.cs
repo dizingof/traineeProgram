a9fcc6888d4da4cc9c041d92957c08e7d4a7dbd9
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-03-10 20:50:48 +0200

    add project game life

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Cell
+    {
+        bool sostoyanie;
+        int X;
+        int Y;
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Cell
+    {
+        public Position Position { get; private set; }
+        public bool IsAlive { get; set; }
+        public Cell[] Neighbours { get; set; }
+
+        public Cell(Position position, bool isAlive = false)
+        {
+            Position = position;
+            IsAlive = isAlive;
+        }
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ GameOfLife/Grid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Grid
+    {
+        int dlina = 10;
+        int shirina = 10;
+    }
+    public class Universe
+    {
+        private Cell[,] _space;
+        private readonly int _spaceMeasurement;
+
+        public Universe(int spaceMeasurement)
+        {
+            _spaceMeasurement = spaceMeasurement;
+            SetEmptySpace();
+        }
+    }
+}
