852d4203b46eebb21ed3df075c07b1e16723b03f
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-04-08 22:43:16 +0300

    add client server project

---
+++ ClientServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener serverSocket = new TcpListener(IPAddress.Any, 7000);
+                Console.WriteLine("Server Started");
+                serverSocket.Start();
+                while (true)
+                {
+                    TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                    NetworkStream stream = clientSocket.GetStream();
+                    string message = "<H1>Hello!</H1>";
+                    byte[] bytes = Encoding.ASCII.GetBytes(message);
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Flush();
+                    Console.WriteLine("Send message:" + message);
+                    clientSocket.Close();
+                }
+                serverSocket.Stop();
+                Console.WriteLine("Server Stopped");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
