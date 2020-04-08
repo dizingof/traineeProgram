08f879ff95a4e51f76dcc4f4e446ab8dafd825ec
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-04-09 00:27:40 +0300

    add client

---
+++ Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				TcpClient client = new TcpClient("127.0.0.1", 7000);
+				Console.WriteLine("Client connected");
+				NetworkStream stream = client.GetStream();
+				byte[] bytes = new byte[256];
+				stream.Read(bytes, 0, bytes.Length);
+				string answer = Encoding.ASCII.GetString(bytes);
+				Console.WriteLine(answer);
+				client.Close();
+				Console.WriteLine("Client closed");
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine(ex.Message);
+			}
+			Console.ReadLine();
+        }
+    }
+}
---
+++ test1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myslovo = "dibil";
+            for (int i = 1; i <= myslovo.Length; i++)
+            {
+                
+                if (i == myslovo.Length) 
+                {
+                    Console.WriteLine(i);
+                }
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
